71fbbae141b115b80ddc8168a1e767a9b2feca55
Otavio <otavioaugustocassiano@gmail.com>
2024-02-13 11:07:03 -0300

    Adicionado cálculo para saber a porcentagem que falta

--- src/Calculus/Calculation.java
+++ src/Calculus/Calculation.java
@@ -1,6 +1,7 @@
 package Calculus;
 
 public class Calculation {
+    //?This class is responsible for all calculations
     private double totalElements;
     private double samples;
     private double percentage;
@@ -17,15 +18,20 @@ public class Calculation {
         return samples;
     }
 
-    public double conversion(double totalElements, double samples){
+    private double conversion(double totalElements, double samples){
         percentage = (samples*100)/totalElements;
         return percentage;
     }
 
+    private double remaining(double percentage){
+        double remaining = 100 - percentage;
+        return remaining;
+    }
+
     @Override
     public String toString(){
-        return "Percentage: "+String.format("%.2f", conversion(totalElements, samples))
-        +"%";
+        return "Percentage: "+String.format("%.2f", conversion(totalElements, samples)) + "%"
+        +"\nRemaining: "+String.format("%.2f", remaining(percentage)) + "%";
     }
 
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What is the total number of elements(Maybe the number of steps? Or the total distance?) ");
+        System.out.print("What is the total number of elements (Maybe the number of steps? Or the total distance?) ");
         double totalElements = scanner.nextDouble();
         scanner.nextLine();
 
-        System.out.print("What is the number of sample? (How many steps did you make? What distance did you walk?) ");
+        System.out.print("What is the number of samples (How many steps did you make? What distance did you walk?) ");
         double samples = scanner.nextDouble();
         scanner.nextLine();
 
